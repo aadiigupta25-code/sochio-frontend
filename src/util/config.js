c8fad18b143bfff85ddb28c23d3f111d962d6f34
aadiigupta25-code <aadiigupta25@gmail.com>
2025-11-19 10:06:56 +0530

    Production config

--- src/util/config.js
+++ src/util/config.js
@@ -1,3 +1,3 @@
-export const baseURL = process.env.REACT_APP_API_URL || "https://sochio-admin-backend.onrender.com/";
+export const baseURL = process.env.REACT_APP_API_URL || "https://sochio-backend.onrender.com/";
 export const secretKey = process.env.REACT_APP_SECRET_KEY || "sochio-api-secret-2024";
 export const projectName = process.env.REACT_APP_PROJECT_NAME || "Sochio";
